dfd8959560a8b1b5bb9575c72844ca7bc7073118
Заровецкий Глеб <gzarovetsky@yandex.ru>
2022-03-20 18:46:17 +0500

    final deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось сделать deploy через GitHub actions!')
 
 
 def second_page(request):
